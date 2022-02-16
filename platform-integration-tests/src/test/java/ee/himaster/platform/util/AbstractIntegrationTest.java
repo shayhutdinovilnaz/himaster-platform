91a8ea5dabcb55f6b163afb0c7ce23fef6d49fa8
ilnaz.shaikhutdinov <ilnaz.shaikhutdinov@gamesys.com>
2022-02-17 00:04:01 +0300

    MASTER: Refactoring

--- platform-integration-tests/src/test/java/ee/himaster/platform/util/AbstractIntegrationTest.java
+++ platform-integration-tests/src/test/java/ee/himaster/platform/util/AbstractIntegrationTest.java
@@ -1,12 +1,12 @@
 package ee.himaster.platform.util;
 
-import ee.himaster.platform.web.application.configuration.PlatformApplicationConfiguration;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import ee.himaster.platform.web.application.configuration.PlatformApplicationConfiguration;
 
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
