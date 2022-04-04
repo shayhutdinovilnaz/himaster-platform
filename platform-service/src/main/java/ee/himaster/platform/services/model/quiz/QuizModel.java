4c6d7917c03b15b9b5766bd0128426fc7aa724e5
shaikhutdinov.ilnaz <ilnaz-92@yandex.ru>
2022-04-05 00:32:01 +0300

    master - The tests are added.

--- platform-service/src/main/java/ee/himaster/platform/services/model/quiz/QuizModel.java
+++ platform-service/src/main/java/ee/himaster/platform/services/model/quiz/QuizModel.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Entity(name = "quiz")
 public class QuizModel extends ItemModel {
 
+    @Column(name = "current_step")
     private int currentStep;
 
     @Column(name = "user_id")
